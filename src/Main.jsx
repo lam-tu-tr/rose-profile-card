1575b849d3bb06193876c1ab2d7df7e9dde66640
lam <lam@lams-popos.lan>
2022-12-04 09:23:09 -0800

    Centered correctly

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
             <h2>About</h2>
             <p>Roseanne Park (born 11 February 1997), known mononymously as Rosé (Korean: 로제), is a Korean-New Zealand singer and dancer based in South Korea. </p>
             <h2>Facts</h2>
-            <p>Rosé placed first in YG auditions in Australia. Pre-debut, Rosé used to be a cheerleader in Australia. Hobbies: playing guitar, drawing, and riding the bicycle.</p>
+            <p>Hobbies: playing guitar, drawing, and riding the bicycle.</p>
         </div>
     )
 }
