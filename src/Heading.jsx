e34f61aa15ce1e5324a74b72d7db0acf1d0b27a3
lam <lam@lams-popos.lan>
2022-12-03 23:35:19 -0800

    Static asset using imported object instead of url

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Mail from '/src/assets/Mail.png'
 import Linkedin from '/src/assets/Linkedin.png'
 
+import Rose from '/src/assets/Rose.png'
+
 export default function Heading() {
     return (
         <div>
-            <img src="./src/assets/Rose.png" alt='rose' width="320px" id ="rose"/>
+            <img src={Rose} alt='rose' id ="rose"/>
             <h1>Roseanne Park</h1>
             <h4>Blackpink</h4>
             <h5>Main Vocalist, Lead Dancer</h5>
